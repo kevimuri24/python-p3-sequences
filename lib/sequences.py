7dc58267452ca0e4f8d2c424323ee26900bb16bf
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-09-27 15:03:21 -0400

    Finished Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    my_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    my_sequence = []
+    for i in range(length):
+        my_sequence.append(my_numbers[i]) 
+        
+    print(my_sequence)
+        
+
+print_fibonacci(9)
